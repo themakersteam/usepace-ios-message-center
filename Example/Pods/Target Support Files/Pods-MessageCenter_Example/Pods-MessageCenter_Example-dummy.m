f0330fc1a014f25a40ac953a833fc4c62de4a31a
cEliteDev <iagilelite@gmail.com>
2018-11-07 11:13:15 -0800

    create pod library and examle app

---
+++ Example/Pods/Target Support Files/Pods-MessageCenter_Example/Pods-MessageCenter_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessageCenter_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MessageCenter_Example
+@end
---
+++ MessageCenter/Classes/Client/ClientProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  ClientInterface.swift
+//  MessageCenter
+//
+//  Created by iDev on 11/1/18.
+//  Copyright © 2018 usepace. All rights reserved.
+//
+
+import Foundation
+
+public protocol ClientProtocol {
+    func connect(connectionRequest: ConnectionRequest, connection: ConnectionProtocol)
+    func join(chatId: String)
+    func disconnect(disconnectInterface: DisconnectionProtocol)
+    func handleNotification(next: AnyClass,  icon: Int, title: String, remoteMessage: AnyClass, messages: NSArray)
+    func isConnected() -> Bool
+}
