fff7344190c87ed57f85c88be2651cc760634bc9
mohamedg <mohamed.alghzawi@hungerstation.com>
2018-11-27 17:45:13 +0300

    Added mapSetting and completed implementation..

--- MessageCenter/Classes/Views/Location/SelectLocationViewController.swift
+++ MessageCenter/Classes/Views/Location/SelectLocationViewController.swift
@@ -11,13 +11,19 @@ import MapKit
 
 public class SelectLocationViewController: UIViewController {
     
-
+    
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapButtonsContainer: UIStackView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var mapSettingsView: UIView!
     
     @IBOutlet weak var centerPin: UIImageView!
     @IBOutlet weak var mapSettingBtn: UIButton!
     @IBOutlet weak var myLocationBtn: UIButton!
     
+    
+    var isShowingMapSettings: Bool = false
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +33,62 @@ public class SelectLocationViewController: UIViewController {
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
+        
+    }
+    
+    @IBAction func showMapSettings(_ sender: Any) {
+        if isShowingMapSettings {
+            return
+        }
+        
+        isShowingMapSettings = true
+        
+        var newY = CGFloat(0)
+        if #available(iOS 11.0, *) {
+            newY = self.view.safeAreaInsets.bottom
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.mapButtonsContainer.alpha = CGFloat(0)
+            self.mapView.alpha = CGFloat(0.8)
+            self.mapSettingsView.transform = CGAffineTransform(translationX: 0, y: newY)
+            
+        }
+    }
+    
+    @IBAction func dismissSettings(_ sender: Any) {
+        
+        if !isShowingMapSettings {
+            return
+        }
+        isShowingMapSettings = false
+        
+        UIView.animate(withDuration: 0.2) {
+            
+            self.mapButtonsContainer.alpha = CGFloat(1)
+            self.mapView.alpha = CGFloat(1)
+            self.mapSettingsView.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height)
+            
+        }
     }
     
     @IBAction func cencel(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func didTapMapView() {
+        if isShowingMapSettings {
+            self.dismissSettings(self)
+            
+        }
+        
+    }
+    
+    @objc func didHoldMapView(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .ended {
+            
+            print("Hoold")
+        }
     }
 }
 
@@ -39,21 +96,34 @@ public class SelectLocationViewController: UIViewController {
 extension SelectLocationViewController {
     
     func prepareView() {
-
+        
+        let mapTapGesture = UITapGestureRecognizer(target: self, action: #selector(self.didTapMapView))
+        let mapLongPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(self.didHoldMapView))
+        
+        
+        self.mapView.addGestureRecognizer(mapTapGesture)
+        self.mapView.addGestureRecognizer(mapLongPressGesture)
+        
+        
+        mapSettingsView.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height)
+        
+        
         centerPin.image = UIImage(named: "btn_selected", in: Bundle(for: MessageCenter.self), compatibleWith: nil)
         
-        self.applyMapButtonsStyle(to: myLocationBtn, corners: [.bottomRight, .bottomLeft])
+        applyCornerRadius(to: self.mapSettingsView, corners: [.topRight, .topLeft])
         
-        self.applyMapButtonsStyle(to: mapSettingBtn, corners: [.topLeft, .topRight])
+        self.applyCornerRadius(to: myLocationBtn, corners: [.bottomRight, .bottomLeft])
+        
+        self.applyCornerRadius(to: mapSettingBtn, corners: [.topLeft, .topRight])
         
     }
     
-    func applyMapButtonsStyle(to btn: UIButton, corners: UIRectCorner) {
+    func applyCornerRadius(to view: UIView, corners: UIRectCorner) {
         
-        let path = UIBezierPath(roundedRect: btn.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: 8, height: 8))
+        let path = UIBezierPath(roundedRect: view.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: 8, height: 8))
         let mask = CAShapeLayer()
         mask.path = path.cgPath
-        btn.layer.mask = mask
+        view.layer.mask = mask
         
     }
     
